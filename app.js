cc43b94dc1f2c53436ade82cfc16cd56e4afcdb8
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 18:16:43 +0200

    make it better

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const gateway = new selfcore.Gateway(
 );
 
 gateway.on("message", (m) => {
-  if (m.channel_id === "974767565895270430" || m.channel_id === "1003744266964701294" ) {
+  if (m.channel_id === "1003744870793494659" || m.channel_id === "1003744947578605769" || m.channel_id === "1017001782850891806" || m.channel_id === "1017001615145844797" || m.channel_id === "998554678407213066") {
     let content = m.content ? m.content : { embeds: [{ image:  m.image }, m.embeds[0]]};
 
 
 
 
      client.sendWebhook(
-      "https://ptb.discord.com/api/webhooks/1032297645567397918/6rBBKYONos0VQKoqIV70Ww6yI7odVXgKHzTSttvqnUPYjyUxspoZZ4_Udrbiwts5eIwc",
+      "https://ptb.discord.com/api/webhooks/1032321186308292631/viHXhNuJTGUWWzgC1-vwkPLyJJyEdAT1kxdynPg-nedz24BR0hWHSwBB36c9tdIPg5js",
       content, m.image, m.attachments.url
       );
 
